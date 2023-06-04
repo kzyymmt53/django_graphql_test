0a28d90db06069e25ecdfbfb7e0380362395c30b
kzyymmt53 <kzyymmt53@gmail.com>
2023-06-04 14:59:16 +0900

    　モデル追加

--- sns/settings.py
+++ sns/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+    'api.apps.ApiConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +52,23 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+GRAPHQL_JWT = {
+     'JWT_VERIFY_EXPIRATION': True,
+     'JWT_EXPIRATION_DELTA': timedelta(minutes=30),
+}
+
+GRAPHENE = {'SCHEMA': 'sns.schema.schema',
+            'MIDDLEWARE': [
+                'graphql_jwt.middleware.JSONWebTokenMiddleware',
+            ],
+}
+
+AUTHENTICATION_BACKENDS = [
+  'graphql_jwt.backends.JSONWebTokenBackend',
+  'django.contrib.auth.backends.ModelBackend',
 ]
 
 ROOT_URLCONF = 'sns.urls'
